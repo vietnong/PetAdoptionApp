e245610438833c8eee961aa4cf4075cc7f53cd1c
longnvb01476 <longnvb01476@gmail.com>
2021-11-26 08:44:33 +0700

    add react dependencies

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+import React from "react";
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
 
-const App = () => {
+export const App = () => {
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement("h1", {}, "Adopt Me!123"),
     React.createElement(Pet, {
       name: "Luna",
       animal: "Dog",
