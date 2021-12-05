a5d5da185d25401f0062a3bf46fe5b40387bcfe9
longnvb01476 <longnvb01476@gmail.com>
2021-12-05 20:34:13 +0700

    use Typescript with modal component

---
+++ src/Modal.tsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useRef, MutableRefObject, FunctionComponent } from "react";
+import { createPortal } from "react-dom";
+
+const modalRoot = document.getElementById("modal");
+const Modal : FunctionComponent = ({ children }) => {
+  const elRef : MutableRefObject<HTMLDivElement | null> = useRef(null);
+  if (!elRef.current) {
+    elRef.current = document.createElement("div");
+  }
+  useEffect(() => {
+    if(!modalRoot || !elRef.current){
+      return;
+    }
+    modalRoot.appendChild(elRef.current);
+    return () =>{
+      elRef.current && modalRoot.removeChild(elRef.current);
+    } 
+  }, []);
+  return createPortal(<div>{children}</div>, elRef.current);
+};
+export default Modal;
